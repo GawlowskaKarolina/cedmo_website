360058f9521c83889c1821aa515ad315e7ab2310
Karolina Gawlowska <karolinka.gawlowska@seznam.cz>
2025-08-15 14:53:50 +0200

    zk2

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-// src/App.tsx
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import Theme from './Theme.ts';
 
@@ -12,29 +11,23 @@ import Index from './pages/Index';
 import About from './pages/About';
 import Publications from './pages/Publications';
 import Cookies from './pages/Cookies';
-
-// Přidáme import cookie consent
 import CookieConsent from './components/CookieConsent';
 
 const App: React.FC = () => {
   return (
     <ThemeProvider theme={Theme}>
       <CssBaseline />
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/trends" element={<Trends />} />
-          <Route path="/index" element={<Index />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/publications" element={<Publications />} />
-          <Route path="/cookies" element={<Cookies />} />
-        </Routes>
-        <Footer />
-
-        {/* Zde přidáme cookie banner */}
-        <CookieConsent />
-      </Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/trends" element={<Trends />} />
+        <Route path="/index" element={<Index />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/publications" element={<Publications />} />
+        <Route path="/cookies" element={<Cookies />} />
+      </Routes>
+      <Footer />
+      <CookieConsent />
     </ThemeProvider>
   );
 };
